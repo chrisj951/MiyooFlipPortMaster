8ad4c785330efcc705414ca790f4456acc20cba2
Christopher Jacobs <chrisj951@gmail.com>
2025-05-17 18:37:14 -0700

    Remove reliance on .tmp_update so it works on purely stock

---
+++ pyui/miyoo/flip/setup_mounts.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+if [ ! -d /mnt/SDCARD/Saves/userdata-flip ]; then
+    mkdir /mnt/SDCARD/Saves/userdata-flip
+    cp -R /userdata/* /mnt/SDCARD/Saves/userdata-flip
+    mkdir -p /mnt/SDCARD/Saves/userdata-flip/bin
+    mkdir -p /mnt/SDCARD/Saves/userdata-flip/bluetooth
+    mkdir -p /mnt/SDCARD/Saves/userdata-flip/cfg
+    mkdir -p /mnt/SDCARD/Saves/userdata-flip/localtime
+    mkdir -p /mnt/SDCARD/Saves/userdata-flip/timezone
+    mkdir -p /mnt/SDCARD/Saves/userdata-flip/lib
+    mkdir -p /mnt/SDCARD/Saves/userdata-flip/lib/bluetooth
+fi
+
+/mnt/SDCARD/pyui/miyoo/flip/recombine_large_files.sh > /mnt/SDCARD/pyui/logs/recombine_large_files.log  2>&1
+/mnt/SDCARD/pyui/miyoo/flip/mount_32bit_libs.sh > /mnt/SDCARD/pyui/logs/mount_32bit_libs.log  2>&1
+/mnt/SDCARD/pyui/common/mount_libs.sh > /mnt/SDCARD/pyui/logs/mount_libs.log  2>&1
+/mnt/SDCARD/pyui/miyoo/flip/setup_32bit_libs.sh > /mnt/SDCARD/pyui/logs/setup_32bit_libs.log  2>&1
+/mnt/SDCARD/pyui/miyoo/flip/bind_glibc.sh > /mnt/SDCARD/pyui/logs/bind_glibc.log  2>&1
+
+# PortMaster ports location
+mkdir -p /mnt/SDCARD/Roms/PM/ports/ 
+mount --bind /mnt/SDCARD/Roms/PM/ /mnt/SDCARD/Roms/PM/ports/
+	
+# PortMaster looks here for config information which is device specific
+mount --bind /mnt/SDCARD/pyui/miyoo/flip/root/ /root 
+
